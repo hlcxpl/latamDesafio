d7bcccf830c463c0f0bba747fb750050d98dca30
Luis <30819865+hlcxpl@users.noreply.github.com>
2022-08-09 16:19:46 -0400

    last modification

--- Desafio Latam/Modulo 3 Desafio - 5/assets/js/funciones.js
+++ Desafio Latam/Modulo 3 Desafio - 5/assets/js/funciones.js
@@ -4,9 +4,9 @@ btnAgregar = document.querySelector("#agregarTareas")
 tbody = document.querySelector("tbody")
 span = document.querySelector('#cuenta-tareas')
 span2 = document.querySelector('#realizadas')
-checkBox = document.querySelectorAll('.status')
 
 
+//ARREGLO GLOBAL CON 3 TAREAS POR DEFAULT
 const tareas = [
     { id: 1000, nombre: 'comer a la hora', estado: false },
     { id: 3000, nombre: 'ir a fiesta', estado: true },
@@ -14,7 +14,8 @@ const tareas = [
 ]
 
 
-
+// EVENTO CLICK PARA EL BOTON DE AGREGAR TAREA TOMA EL VALOR DEL INPUT Y LE ASIGNA UN ID Y UN ESTADO POR DEFAULT
+// EN FALSO EN UN ARREGLO
 btnAgregar.addEventListener('click', function agregarValor() {
     /* Agregamos el invitado al arreglo */
     nuevoTarea = { id: Date.now(), nombre: input.value, estado: false }
@@ -26,11 +27,11 @@ btnAgregar.addEventListener('click', function agregarValor() {
 
 //FUNCION RENDER TAREA CON COMPROBACION DE ESTADO PARA ASIGNARLE EL VALOR CHECKED 
 //Y ASIGNARLE UNA FUNCION DE CAMBIO DE ESTADO A LOS TRUE Y SOLO LA FUNCION DE CAMBIO 
-//DE ESTADO A LOS FALSE 
+//DE ESTADO A LOS FALSE RECIBE COMO PARAMETRO EL ARREGLO Y LA ETIQUETA DONDE SE QUIERE IMPRIMIR
 function renderTareas(arreglos, etiqueta) {
     let html = ""
-    for (arreglo of arreglos) {
-        if (arreglo.estado == true) {
+    for(arreglo of arreglos) {
+        if(arreglo.estado == true) {
             variable = 'checked'
             html += `<tr><td class="px-5">${arreglo.id}</td><td class="px-5">${arreglo.nombre}</td>
         <td class="px-5">
@@ -54,35 +55,39 @@ function renderTareas(arreglos, etiqueta) {
         <td class="px-5"><button class="btn btn-light text-dark" onclick ="borrar(${arreglo.id})"> x </button><td></tr>`
         }
     }
+    //IMPRIME EL TEMPLATE EN LA ETIQUETA QUE PASEMOS COMO PARAMETROS
     etiqueta.innerHTML = html;
+    //CUENTA TODAS LAS TAREAS
     span.innerHTML = tareas.length
+    //CUENTA LAS TAREAS EN TRUE
     contadorCheck(tareas)
 }
 
 
 
-
-//BUSCAS EL ESTADO FALSE A TRAVEZ DE LA COMPROBACION QUE SE HACE EN LA FUNCION RENDER TAREA - LO BUSCAR EL ID Y CAMBIA EL ESTADO A FALSE
+//BUSCAS EL ESTADO TRUE A TRAVEZ DE LA COMPROBACION QUE SE HACE EN LA FUNCION RENDER TAREA - LO BUSCA POR EL ID Y 
+//CAMBIA EL ESTADO A FALSE
 function cambioEstadoTrue(id) {
     const index = tareas.findIndex(tarea => tarea.id == id)
     tareas[index].estado = false
     renderTareas(tareas, tbody)
     console.log(tareas)
 }
-//BUSCAS EL ESTADO FALSE A TRAVEZ DE LA COMPROBACION QUE SE HACE EN LA FUNCION RENDER TAREA - LO BUSCAR EL ID Y CAMBIA EL ESTADO A TRUE
+//BUSCAS EL ESTADO FALSE A TRAVEZ DE LA COMPROBACION QUE SE HACE EN LA FUNCION RENDER TAREA - LO BUSCA POR EL ID Y 
+//CAMBIA EL ESTADO A TRUE
 function cambioEstadoFalse(id) {
     const index = tareas.findIndex(tarea => tarea.id == id)
     tareas[index].estado = true
     renderTareas(tareas, tbody)
     console.log(tareas)
 }
-
-// CONTADOR DE TAREA REALIZADA ATRAVEZ DEL ESTADO
+// CONTADOR DE TAREA REALIZADA ATRAVEZ DEL ESTADO - BUSCA LOS ESTADOS EN TRUE DEL ARREGLO Y LOS CUENTA A TRAVES
+// DEL METODO FILTER
 function contadorCheck(arreglo) {
     const checkToDo = arreglo.filter(tarea => tarea.estado == true)
     span2.innerHTML = checkToDo.length
 }
-// BORRAR LA TAREA CON BUTTON A TRAVEZ DEL ID Y EL INDICE
+// BORRAR LA TAREA CON BUTTON A TRAVEZ DEL ID Y EL INDICE UTILIZANDO EL METODO SPLICE
 function borrar(id) {
     const index = tareas.findIndex((ele) => ele.id == id)
     tareas.splice(index, 1)
